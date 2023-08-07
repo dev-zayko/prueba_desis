33b583d1b8eb9be80867c0772992710ffa0230da
zayko-dev <pablo.rodriguez96k@gmail.com>
2023-08-07 15:03:20 -0400

    Fix Documentacion

--- functions/get_data.php
+++ functions/get_data.php
@@ -1,7 +1,29 @@
 <?php
+/**
+ * Incluye el archivo de configuración de la base de datos.
+ */
 include '../config/database.php';
 
+/**
+ * Función para obtener datos de la base de datos según la tabla especificada.
+ * @param string $table - El nombre de la tabla desde donde se obtendrán los datos.
+ */
+function getData($table)
+{
+    // Conectarse a la base de datos
+    $conn = connect();
+
+    // Preparar y ejecutar la consulta para obtener todos los datos de la tabla
+    $stmt = $conn->query('SELECT * FROM ' . $table);
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Devolver los datos en formato JSON
+    echo json_encode($data);
+}
+
+// Verificar si la solicitud es de tipo GET
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Verificar si se proporcionó el parámetro 'action' en la solicitud GET
     if (isset($_GET['action'])) {
         $action = $_GET['action'];
         switch ($action) {
@@ -16,12 +38,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 break;
         }
     }
-}
-
-function getData($table)
-{
-    $conn = connect();
-    $stmt = $conn->query('SELECT * FROM ' . $table);
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($data);
 }
--- functions/post_data.php
+++ functions/post_data.php
@@ -1,20 +1,22 @@
 <?php
+/**
+ * Incluye el archivo de configuración de la base de datos.
+ */
 include '../config/database.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST)) {
-        $request = $_POST;
-        check_duplicate($request);
-    }
-}
-
-
+/**
+ * Función para manejar la solicitud POST y verificar si el usuario ya ha votado.
+ * Si no ha votado, realiza la inserción de los datos del usuario y su voto.
+ * @param array $request - Los datos enviados en la solicitud POST.
+ */
 function check_duplicate($request)
 {
+    // Verifica si el RUT está presente en los datos de la solicitud POST
     if (isset($request['rut'])) {
         $conn = connect();
         $rut = $request['rut'];
 
+        // Consulta para contar la cantidad de votos asociados a un RUT en la tabla 'vote' vinculada a la tabla 'user'
         $stmt = $conn->prepare('SELECT COUNT(*) as count FROM vote 
                                 INNER JOIN user ON vote.user_id = user.id 
                                 WHERE user.rut = :rut');
@@ -22,21 +24,28 @@ function check_duplicate($request)
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        // Verificar si el RUT ya ha votado
         $exists = $result['count'] > 0;
 
         if ($exists) {
+            // Si el RUT ya ha votado, se devuelve una respuesta JSON indicando que ya existe un voto asociado a ese RUT
             echo json_encode(array('status' => 'exists'));
-            //echo json_encode(array('exists' => true));
         } else {
-
+            // Si el RUT no ha votado, se procede a realizar la inserción del usuario y su voto
             perfom_insert($request);
             echo json_encode(array('status' => 'success'));
         }
     } else {
+        // Si no se proporciona el RUT en la solicitud POST, se devuelve una respuesta JSON de error
         echo json_encode(array('error' => 'ERROR'));
     }
 }
 
+/**
+ * Función para insertar los datos del usuario en la tabla 'user'.
+ * @param array $data - Los datos del usuario a insertar.
+ * @return int - El ID del usuario insertado.
+ */
 function insert_user($data)
 {
     $conn = connect();
@@ -47,22 +56,31 @@ function insert_user($data)
 
     $stmt->execute();
 
-    // Obtenemos el ultimo id creado
+    // Obtenemos el último ID creado (ID del usuario insertado)
     $lastInsertedId = $conn->lastInsertId();
 
     return $lastInsertedId;
 }
 
+/**
+ * Función para realizar la inserción del usuario y su voto en la base de datos.
+ * @param array $data - Los datos del usuario y su voto a insertar.
+ */
 function perfom_insert($data)
 {
+    // Insertamos los datos del usuario en la tabla 'user' y obtenemos el ID del usuario insertado
     $userInsertedId = insert_user($data);
 
+    // Verificamos los checkboxes y convertirmos en 0 o 1 según estén marcados o no
     $checkWeb = isset($data['checkWeb']) ? 1 : 0;
     $checkSocial = isset($data['checkSocial']) ? 1 : 0;
     $checkFriend = isset($data['checkFriend']) ? 1 : 0;
     $checkTv = isset($data['checkTv']) ? 1 : 0;
+
+    // Obtenemos el ID de la comuna seleccionada desde el formulario
     $commune_id = $data['communes'];
 
+    // Realizamos la inserción del voto del usuario en la tabla 'vote'
     $conn = connect();
     $stmt = $conn->prepare('INSERT INTO vote (user_id, candidate_id, web, social, friend, tv, commune_id) VALUES (:user_id, :candidate_id, :web, :social, :friend, :tv, :commune_id)');
     $stmt->bindParam(':user_id', $userInsertedId);
